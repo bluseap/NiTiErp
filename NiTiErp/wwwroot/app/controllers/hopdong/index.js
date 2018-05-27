56bca9d95b6571d06432ed64337a1bdae2ad417f
le nguyen <khoinguyenaglx@gmail.com>
2018-05-27 15:47:31 +0700

    026HRM

---
+++ NiTiErp/wwwroot/app/controllers/hopdong/index.js
@@ -0,0 +1,68 @@
+﻿var hopdongController = function () {
+
+    //var images = [];
+
+    this.initialize = function () {
+
+        loadData();
+
+        registerEvents();
+    }
+
+    function registerEvents() {
+        //Init validation
+        //$('#frmMainLyLich').validate({
+        //    errorClass: 'red',
+        //    ignore: [],
+        //    lang: 'vi',
+        //    rules: {
+        //        txtHoVaTen: { required: true },
+        //        ddlGioiTinh: { required: true },
+        //        txtHeSoLuongCoBan: {
+        //            required: true,
+        //            number: true
+        //        },
+        //        txtLuongCoBan: {
+        //            required: true,
+        //            number: true
+        //        }
+        //    }
+        //});
+
+        $("#btn-create").on('click', function () {
+            resetFormMaintainance();
+            $('#modal-add-edit-HopDong').modal('show');
+        });
+
+    }
+
+    function resetFormMaintainance() {
+        //$('#hidLyLichId').val(0);
+
+        //$('#txtSoTheNhanVien').val('');
+        //$('#txtHoVaTen').val('');
+
+
+        //$('#txtPriceM').val('0');
+        //$('#txtImageM').val('');    
+        //CKEDITOR.instances.txtContentM.setData('');
+        //$('#ckStatusM').prop('checked', true);
+        //$('#ckHotM').prop('checked', false);
+        //$('#ckShowHomeM').prop('checked', false);
+    }
+
+    function loadData() {
+
+        //var gioitinh = [{ value: "1", ten: "Nam" }, { value: "0", ten: "Nữ" }];
+        //var render = "";
+        //for (var i = 0; i < gioitinh.length ; i++) {
+        //    render += "<option value='" + gioitinh[i].value + "'>" + gioitinh[i].ten + "</option>";
+        //}
+        //$('#ddlGioiTinh').html(render);
+
+
+    }
+
+
+
+}
