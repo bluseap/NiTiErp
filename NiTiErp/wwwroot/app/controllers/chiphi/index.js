2c6e8c36e27706b611ec4bd4769524d6745979d7
le nguyen <khoinguyenaglx@gmail.com>
2018-10-12 15:13:34 +0700

    039crm

---
+++ NiTiErp/wwwroot/app/controllers/chiphi/index.js
@@ -0,0 +1,17 @@
+﻿var chiphiController = function () {
+
+    var userCorporationId = $("#hidUserCorporationId").val();
+
+
+    this.initialize = function () {
+
+        registerEvents();
+
+
+    }
+
+    function registerEvents() {
+
+    }
+
+}
---
+++ NiTiErp/wwwroot/app/controllers/chiphidm/index.js
@@ -0,0 +1,17 @@
+﻿var chiphidmController = function () {
+
+    var userCorporationId = $("#hidUserCorporationId").val();
+
+
+    this.initialize = function () {
+
+        registerEvents();
+
+
+    }
+
+    function registerEvents() {
+
+    }
+
+}
