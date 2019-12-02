edad94a28b8eb0b20f227879c515c75fad81ee0a
le nguyen <khoinguyenaglx@gmail.com>
2019-12-02 10:36:58 +0700

    059pwDoc

--- NiTiAPI.WebErp/wwwroot/cnews-app/controllers/home/index.js
+++ NiTiAPI.WebErp/wwwroot/cnews-app/controllers/home/index.js
@@ -11,6 +11,18 @@
 
     function registerEvents() {
 
+        $('body').on('click', '.imgViewPostId', function (e) {
+            e.preventDefault();
+            var postId = $(this).data('id');
+
+            alert(postId);
+        });
+        $('body').on('click', '.titleViewPostId', function (e) {
+            e.preventDefault();
+            var postId = $(this).data('id');
+
+            alert(postId);
+        });
         $('body').on('click', '.btnViewPost', function (e) {        
             e.preventDefault();
             var postId = $(this).data('id');
