e4315096bd48b13c7cc7356d510cf1f0fa74aa32
powaDocMobi <khoinguyenaglx@gmail.com>
2020-04-20 09:16:49 +0700

    066podoc

--- NiTiAPI.WebErp/wwwroot/cprice-app/shared/niti.js
+++ NiTiAPI.WebErp/wwwroot/cprice-app/shared/niti.js
@@ -397,12 +397,12 @@
 
 }
 
-$(document).ajaxSend(function(e, xhr, options) {
-    if (options.type.toUpperCase() === "POST" || options.type.toUpperCase() === "PUT") {
-        var token = $('form').find("input[name='__RequestVerificationToken']").val();
-        xhr.setRequestHeader("RequestVerificationToken", token);
-    }
-});
+//$(document).ajaxSend(function(e, xhr, options) {
+//    if (options.type.toUpperCase() === "POST" || options.type.toUpperCase() === "PUT") {
+//        var token = $('form').find("input[name='__RequestVerificationToken']").val();
+//        xhr.setRequestHeader("RequestVerificationToken", token);
+//    }
+//});
 
 //Get the button:
 mybutton = document.getElementById("myBtn");
